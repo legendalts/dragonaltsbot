33b8e45c88c38cc7a1560da407236bf34b8b3c73
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-26 20:14:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,8 +52,9 @@ client.on('message', msg => {
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'getalt') {
-		
-		if (talkedRecently.has(msg.author.id)) {
+	    
+	    if(msg.member.voiceChannel == "getalt"){
+	      if (talkedRecently.has(msg.author.id)) {
 				msg.author.send("Please wait a minute before getting another alt.");
 		} else {
 			msg.channel.send("Check your PM " + msg.author);
@@ -66,6 +67,9 @@ client.on('message', msg => {
 			  talkedRecently.delete(msg.author.id);
 			}, 60000);
 		}
+	    } else {
+	      msg.author.send('Please use !getalt in the #getalt channel of our server.');
+	    }
 	
     }
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
